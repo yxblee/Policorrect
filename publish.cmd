cc081ac96314aa4cda98e078d80e6a3699f44a5b
veron <veronica.lai.vn@gmail.com>
2017-12-02 15:28:54 -0800

    add bot init

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.ProactiveBot.sln -p:DeployOnBuild=true -p:PublishProfile=MLHBot-Web-Deploy.pubxml -p:Password=aDg6cAYG340DqYgXdwGxn3YWM9b3ry7xZ2wf2BdoLZ95iz3sCodZw5NE1efG
+
